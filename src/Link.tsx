0d0919b6118302354e634bb2f7b0069acba0f617
GeronimoAlcantara <galcantarajr@yahoo.com>
2024-02-09 16:05:32 +0000

    Update Link.tsx

--- src/Link.tsx
+++ src/Link.tsx
@@ -4,7 +4,7 @@ import linkedin from "./images/linkedin.jpeg";
 
 function Link() {
 
-  const cv_url='https://docs.google.com/document/d/1tw3jcIqGVt2jOzkyPVOBaw15eL7rJPGV/export?format=docx';
+  const cv_url='https://docs.google.com/document/d/1pJjTW5baVILOp6KX7eu-Xkfvd0gazxSv/export?format=docx';
 
   return (
     <section id="link" className="text-center">
