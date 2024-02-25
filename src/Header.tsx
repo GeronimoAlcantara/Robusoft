ef4b0222bd6c26026852fb7e2c933199b35c06b0
GeronimoAlcantara <galcantarajr@yahoo.com>
2024-02-25 19:06:01 +0000

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
         <header id="home" className="header">
         <div className="overlay"></div>
         <div className="header-content">
-            <img src={robuicon} style={{borderRadius: '10%', opacity: '.6', width: '400px', height: '300px'}}  alt="icon" />
+            <img src={robuicon} style={{borderRadius: '10%', opacity: '.6', width: '350px', height: '275px'}}  alt="icon" />
         </div>      
     </header>
     );
